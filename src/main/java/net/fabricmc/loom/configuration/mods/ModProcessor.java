144fc2eb4b4dbe473c97ee779c822b4a7e1d98b7
DasBabyPixel <43410952+DasBabyPixel@users.noreply.github.com>
2024-02-23 18:39:45 +0100

    proof of concept

--- src/main/java/net/fabricmc/loom/configuration/mods/ModProcessor.java
+++ src/main/java/net/fabricmc/loom/configuration/mods/ModProcessor.java
@@ -28,11 +28,13 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -41,6 +43,8 @@ import java.util.jar.Manifest;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
@@ -117,20 +121,7 @@ public class ModProcessor {
 		return description;
 	}
 
-	private void stripNestedJars(Path path) {
-		// Strip out all contained jar info as we dont want loader to try and load the jars contained in dev.
-		try {
-			ZipUtils.transformJson(JsonObject.class, path, Map.of("fabric.mod.json", json -> {
-				json.remove("jars");
-				return json;
-			}));
-		} catch (IOException e) {
-			throw new UncheckedIOException("Failed to strip nested jars from %s".formatted(path), e);
-		}
-	}
-
-	private void remapJars(List<ModDependency> remapList) throws IOException {
-		final LoomGradleExtension extension = LoomGradleExtension.get(project);
+	private CreateRemapper createRemapper(LoomGradleExtension extension, List<ModDependency> remapList) throws IOException {
 		final MappingConfiguration mappingConfiguration = extension.getMappingConfiguration();
 
 		Set<String> knownIndyBsms = new HashSet<>(extension.getKnownIndyBsms().get());
@@ -139,11 +130,7 @@ public class ModProcessor {
 			knownIndyBsms.addAll(modDependency.getMetadata().knownIdyBsms());
 		}
 
-		TinyRemapper.Builder builder = TinyRemapper.newRemapper()
-				.withKnownIndyBsm(knownIndyBsms)
-				.withMappings(TinyRemapperHelper.create(mappingConfiguration.getMappingsService(serviceManager).getMappingTree(), fromM, toM, false))
-				.renameInvalidLocals(false)
-				.extraAnalyzeVisitor(AccessWidenerAnalyzeVisitorProvider.createFromMods(fromM, remapList));
+		TinyRemapper.Builder builder = TinyRemapper.newRemapper().withKnownIndyBsm(knownIndyBsms).withMappings(TinyRemapperHelper.create(mappingConfiguration.getMappingsService(serviceManager).getMappingTree(), fromM, toM, false)).renameInvalidLocals(false).extraAnalyzeVisitor(AccessWidenerAnalyzeVisitorProvider.createFromMods(fromM, remapList));
 
 		final KotlinClasspathService kotlinClasspathService = KotlinClasspathService.getOrCreateIfRequired(serviceManager, project);
 		KotlinRemapperClassloader kotlinRemapperClassloader = null;
@@ -154,8 +141,7 @@ public class ModProcessor {
 		}
 
 		final Set<InputTag> remapMixins = new HashSet<>();
-		final boolean requiresStaticMixinRemap = remapList.stream()
-				.anyMatch(modDependency -> modDependency.getMetadata().mixinRemapType() == ArtifactMetadata.MixinRemapType.STATIC);
+		final boolean requiresStaticMixinRemap = remapList.stream().anyMatch(modDependency -> modDependency.getMetadata().mixinRemapType() == ArtifactMetadata.MixinRemapType.STATIC);
 
 		if (requiresStaticMixinRemap) {
 			// Configure the mixin extension to remap mixins from mod jars that were remapped with the mixin extension.
@@ -168,11 +154,22 @@ public class ModProcessor {
 
 		final TinyRemapper remapper = builder.build();
 
+		return new CreateRemapper(remapper, remapMixins, kotlinRemapperClassloader, requiresStaticMixinRemap);
+	}
+
+	private ConfigureRemapper configureRemapper(LoomGradleExtension extension, List<ModDependency> remapList, CreateRemapper createRemapper) throws IOException {
+		final TinyRemapper remapper = createRemapper.remapper();
+		final Set<InputTag> remapMixins = createRemapper.remapMixins();
+
 		remapper.readClassPath(extension.getMinecraftJars(MappingsNamespace.INTERMEDIARY).toArray(Path[]::new));
 
 		final Map<ModDependency, InputTag> tagMap = new HashMap<>();
-		final Map<ModDependency, OutputConsumerPath> outputConsumerMap = new HashMap<>();
-		final Map<ModDependency, Pair<byte[], String>> accessWidenerMap = new HashMap<>();
+		final Map<InputTag, List<InputTag>> nestedMap = new HashMap<>();
+		final Map<InputTag, OutputConsumerPath> outputConsumerMap = new HashMap<>();
+		final Map<InputTag, Path> tempFiles = new HashMap<>();
+		final Map<InputTag, String> tempFileNames = new HashMap<>();
+		final Map<InputTag, String> nestedJarPaths = new HashMap<>();
+		final Map<InputTag, Pair<byte[], String>> accessWidenerMap = new HashMap<>();
 
 		for (RemapConfigurationSettings entry : extension.getRemapConfigurations()) {
 			for (File inputFile : entry.getSourceConfiguration().get().getFiles()) {
@@ -191,7 +188,7 @@ public class ModProcessor {
 			// Note: this is done at a jar level, not at the level of an individual mixin config.
 			// If a mod has multiple mixin configs, it's assumed that either all or none of them have refmaps.
 			if (info.getMetadata().mixinRemapType() == ArtifactMetadata.MixinRemapType.STATIC) {
-				if (!requiresStaticMixinRemap) {
+				if (!createRemapper.requiresStaticMixinRemap) {
 					// Should be impossible but stranger things have happened.
 					throw new IllegalStateException("Was not configured for static remap, but a mod required it?!");
 				}
@@ -203,27 +200,86 @@ public class ModProcessor {
 			remapper.readInputsAsync(tag, info.getInputFile());
 			tagMap.put(info, tag);
 
+			// Add transitive dependencies
+			LinkedList<Pair<Path, InputTag>> pathsToCheck = new LinkedList<>();
+			pathsToCheck.add(new Pair<>(info.getInputFile(), tag));
+
+			while (!pathsToCheck.isEmpty()) {
+				Pair<Path, InputTag> pair = pathsToCheck.removeFirst();
+				Path path = pair.left();
+				InputTag pathTag = pair.right();
+				JsonObject json = ZipUtils.unpackGson(path, "fabric.mod.json", JsonObject.class);
+
+				if (json.has("jars")) {
+					JsonArray jars = json.getAsJsonArray("jars");
+
+					for (JsonElement jarElement : jars) {
+						JsonObject jar = jarElement.getAsJsonObject();
+						String filePath = jar.getAsJsonPrimitive("file").getAsString();
+						byte[] bytes = ZipUtils.unpack(path, filePath);
+						Path tempFile = Files.createTempFile("fabric-loom-nested-jar", ".jar");
+						Files.write(tempFile, bytes);
+
+						InputTag nestedTag = remapper.createInputTag();
+						pathsToCheck.add(new Pair<>(tempFile, nestedTag));
+						tempFileNames.put(nestedTag, Paths.get(filePath).getFileName().toString());
+						nestedJarPaths.put(nestedTag, filePath);
+						tempFiles.put(nestedTag, tempFile);
+						remapper.readInputsAsync(nestedTag, tempFile);
+
+						nestedMap.computeIfAbsent(pathTag, t -> new ArrayList<>()).add(nestedTag);
+					}
+				}
+			}
+
 			Files.deleteIfExists(getRemappedOutput(info));
 		}
+
+		return new ConfigureRemapper(tagMap, nestedMap, outputConsumerMap, accessWidenerMap, tempFiles, tempFileNames, nestedJarPaths);
+	}
+
+	private void applyRemapperJar(LoomGradleExtension extension, TinyRemapper remapper, Path inputFile, Path destination, Path nestedPrefix, InputTag tag, ConfigureRemapper configureRemapper, Map<InputTag, Path> destinations) throws IOException {
+		project.getLogger().info("Remapping {} to {}", inputFile, destination);
+		destinations.put(tag, destination);
+		OutputConsumerPath outputConsumer = new OutputConsumerPath.Builder(destination).build();
+
+		outputConsumer.addNonClassFiles(inputFile, NonClassCopyMode.FIX_META_INF, remapper);
+		configureRemapper.outputConsumerMap.put(tag, outputConsumer);
+
+		final AccessWidenerUtils.AccessWidenerData accessWidenerData = AccessWidenerUtils.readAccessWidenerData(inputFile);
+
+		if (accessWidenerData != null) {
+			project.getLogger().debug("Remapping access widener in {}", inputFile);
+			byte[] remappedAw = AccessWidenerUtils.remapAccessWidener(accessWidenerData.content(), remapper.getEnvironment().getRemapper());
+			configureRemapper.accessWidenerMap.put(tag, new Pair<>(remappedAw, accessWidenerData.path()));
+		}
+
+		remapper.apply(outputConsumer, tag);
+
+		if (configureRemapper.nestedMap.containsKey(tag)) {
+			for (InputTag nestedTag : configureRemapper.nestedMap.get(tag)) {
+				String fileName = configureRemapper.tempFileNames.get(nestedTag);
+				Path nestedInputFile = configureRemapper.tempFiles.get(nestedTag);
+				Path thisPrefix = nestedPrefix.resolve(fileName + "-jars");
+
+				Path nestedDestination = extension.getFiles().getProjectBuildCache().toPath().resolve("remapped_working").resolve(nestedPrefix.resolve(fileName));
+				applyRemapperJar(extension, remapper, nestedInputFile, nestedDestination, thisPrefix, nestedTag, configureRemapper, destinations);
+			}
+		}
+	}
+
+	private Map<InputTag, Path> applyRemapper(LoomGradleExtension extension, List<ModDependency> remapList, CreateRemapper createRemapper, ConfigureRemapper configureRemapper) throws IOException {
+		final TinyRemapper remapper = createRemapper.remapper;
+		final Map<ModDependency, InputTag> tagMap = configureRemapper.tagMap;
+		final KotlinRemapperClassloader kotlinRemapperClassloader = createRemapper.kotlinRemapperClassloader;
+
+		final Map<InputTag, Path> destinations = new HashMap<>();
 
 		try {
 			// Apply this in a second loop as we need to ensure all the inputs are on the classpath before remapping.
 			for (ModDependency dependency : remapList) {
 				try {
-					OutputConsumerPath outputConsumer = new OutputConsumerPath.Builder(getRemappedOutput(dependency)).build();
-
-					outputConsumer.addNonClassFiles(dependency.getInputFile(), NonClassCopyMode.FIX_META_INF, remapper);
-					outputConsumerMap.put(dependency, outputConsumer);
-
-					final AccessWidenerUtils.AccessWidenerData accessWidenerData = AccessWidenerUtils.readAccessWidenerData(dependency.getInputFile());
-
-					if (accessWidenerData != null) {
-						project.getLogger().debug("Remapping access widener in {}", dependency.getInputFile());
-						byte[] remappedAw = AccessWidenerUtils.remapAccessWidener(accessWidenerData.content(), remapper.getEnvironment().getRemapper());
-						accessWidenerMap.put(dependency, new Pair<>(remappedAw, accessWidenerData.path()));
-					}
-
-					remapper.apply(outputConsumer, tagMap.get(dependency));
+					applyRemapperJar(extension, remapper, dependency.getInputFile(), getRemappedOutput(dependency), dependency.getInputFile().getFileName(), tagMap.get(dependency), configureRemapper, destinations);
 				} catch (Exception e) {
 					throw new RuntimeException("Failed to remap: " + dependency, e);
 				}
@@ -234,22 +290,65 @@ public class ModProcessor {
 			if (kotlinRemapperClassloader != null) {
 				kotlinRemapperClassloader.close();
 			}
+
+			for (Path path : configureRemapper.tempFiles.values()) {
+				Files.delete(path);
+			}
 		}
 
-		for (ModDependency dependency : remapList) {
-			outputConsumerMap.get(dependency).close();
+		return destinations;
+	}
 
-			final Path output = getRemappedOutput(dependency);
-			final Pair<byte[], String> accessWidener = accessWidenerMap.get(dependency);
+	private void completeRemapping(List<ModDependency> remapList, ConfigureRemapper configureRemapper, Map<InputTag, Path> destinations) throws IOException {
+		for (ModDependency dependency : remapList) {
+			InputTag tag = configureRemapper.tagMap.get(dependency);
+			configureRemapper.outputConsumerMap.get(tag).close();
+
+			final Path output = destinations.get(tag);
+			final Pair<byte[], String> accessWidener = configureRemapper.accessWidenerMap.get(tag);
 
 			if (accessWidener != null) {
 				ZipUtils.replace(output, accessWidener.right(), accessWidener.left());
 			}
 
-			stripNestedJars(output);
+			completeNested(configureRemapper, tag, destinations);
+
 			remapJarManifestEntries(output);
 			dependency.copyToCache(project, output, null);
 		}
+	}
+
+	private void completeNested(ConfigureRemapper configureRemapper, InputTag tag, Map<InputTag, Path> destinations) throws IOException {
+		configureRemapper.outputConsumerMap.get(tag).close();
+
+		if (!configureRemapper.nestedMap.containsKey(tag)) {
+			return;
+		}
+
+		for (InputTag inputTag : configureRemapper.nestedMap.get(tag)) {
+			completeNested(configureRemapper, inputTag, destinations);
+		}
+
+		Path path = destinations.get(tag);
+
+		try {
+			for (InputTag inputTag : configureRemapper.nestedMap.get(tag)) {
+				ZipUtils.replace(path, configureRemapper.nestedJarPaths.get(inputTag), Files.readAllBytes(destinations.get(inputTag)));
+			}
+		} catch (RuntimeException | IOException | Error t) {
+			project.getLogger().error("File: ");
+			project.getLogger().error("Failed while completing nesting for {}", destinations.get(tag));
+			throw t;
+		}
+	}
+
+	private void remapJars(List<ModDependency> remapList) throws IOException {
+		final LoomGradleExtension extension = LoomGradleExtension.get(project);
+
+		final CreateRemapper createRemapper = createRemapper(extension, remapList);
+		final ConfigureRemapper configureRemapper = configureRemapper(extension, remapList, createRemapper);
+		final Map<InputTag, Path> destinations = applyRemapper(extension, remapList, createRemapper, configureRemapper);
+		completeRemapping(remapList, configureRemapper, destinations);
 	}
 
 	private static Path getRemappedOutput(ModDependency dependency) {
@@ -266,5 +365,11 @@ public class ModProcessor {
 			manifest.write(out);
 			return out.toByteArray();
 		}));
+	}
+
+	private record CreateRemapper(TinyRemapper remapper, Set<InputTag> remapMixins, KotlinRemapperClassloader kotlinRemapperClassloader, boolean requiresStaticMixinRemap) {
+	}
+
+	private record ConfigureRemapper(Map<ModDependency, InputTag> tagMap, Map<InputTag, List<InputTag>> nestedMap, Map<InputTag, OutputConsumerPath> outputConsumerMap, Map<InputTag, Pair<byte[], String>> accessWidenerMap, Map<InputTag, Path> tempFiles, Map<InputTag, String> tempFileNames, Map<InputTag, String> nestedJarPaths) {
 	}
 }
